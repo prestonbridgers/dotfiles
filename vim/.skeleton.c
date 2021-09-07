83058608bca4bbda2d7796e5299ef04c7895e983
Curt Bridgers <bridgerspc@appstate.edu>
2021-09-06 23:36:41 -0400

    Added submodules for vim plugins

---
+++ vim/.skeleton.c
@@ -0,0 +1,13 @@
+/* [filename.c]
+ * author: Curt Bridgers
+ * email: prestonbridgers@gmail.com
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	
+	return EXIT_SUCCESS;	
+}
---
+++ vim/.skeleton.java
@@ -0,0 +1,13 @@
+/**
+ * [Classname.java]
+ *
+ * @author Curt Bridgers
+*/
+
+public class 
+{
+	public static void main(String[] args)
+	{
+
+	}
+}
